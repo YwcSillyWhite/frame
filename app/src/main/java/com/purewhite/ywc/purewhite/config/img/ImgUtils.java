dd860cc91b6a64373228aac978cc4a6462ffef36
yuwenchao <1075770029@qq.com>
2019-04-17 14:43:46 +0800

    2019新的开始,imgUtils优化

--- app/src/main/java/com/purewhite/ywc/purewhite/config/img/ImgUtils.java
+++ app/src/main/java/com/purewhite/ywc/purewhite/config/img/ImgUtils.java
@@ -1,10 +1,16 @@
 package com.purewhite.ywc.purewhite.config.img;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.view.View;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * 在Android应用中，图片的主要存在方式：
@@ -51,26 +57,91 @@ public class ImgUtils {
         return bitmap;
     }
 
-    /**
-     * 尺寸压缩会改变图片的尺寸，即压缩图片宽度和高度的像素点
-     * @param bitmap
-     * @return
+
+
+    /**尺寸压缩
+     * @param bitmap 要压缩的图片
+     * @param ratio 压缩比例，值越大，图片的尺寸就越小
+     * @param file 压缩的图片保存地址
      */
-//    public static Bitmap size(Bitmap bitmap)
-//    {
-//
-//    }
+    public static void sizeCompressBitmap(Bitmap bitmap,int ratio,File file){
+        if (ratio<=0){
+            return;
+        }
+        Bitmap result=Bitmap.createBitmap(bitmap.getWidth()/ratio,
+                bitmap.getHeight()/ratio, Bitmap.Config.ARGB_8888);
+        Canvas canvas =new Canvas();
+        Rect rect=new Rect(0,0,bitmap.getWidth()/ratio,bitmap.getHeight()/ratio);
+        canvas.drawBitmap(bitmap,null,rect,null);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        // 把压缩后的数据存放到baos中
+        result.compress(Bitmap.CompressFormat.JPEG, 100 ,baos);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(baos.toByteArray());
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
     /**
      *
-     * @param format
      * @param bitmap
+     * @param file
+     * @param format
      * @param quality
-     * @return
      */
-//    public static Bitmap comPress(Bitmap.CompressFormat format, Bitmap bitmap, int quality)
-//    {
-//        bitmap.compress(format,quality,new Arr)
-//    }
+    public static void qualityCompressBitmap(Bitmap bitmap, File file,Bitmap.CompressFormat format, int quality)
+    {
+        //字节数组输出流
+        ByteArrayOutputStream stream =new ByteArrayOutputStream();
+        //图片压缩后把数据放在stream中
+        bitmap.compress(format,quality, stream);
+        try {
+            FileOutputStream fileOutputStream=new FileOutputStream(file);
+            //不断把stream的数据写文件输出流中去
+            fileOutputStream.write(stream.toByteArray());
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /** 采样率压缩
+     * @param filePath 压缩图
+     * @param file 压缩的图片保存地址
+     */
+    public  static void pixeCompressBitmap(String filePath, File file){
+        //采样率，数值越高，图片像素越低
+        int inSampleSize=8;
+        BitmapFactory.Options osts=new BitmapFactory.Options();
+        osts.inSampleSize=inSampleSize;
+        //inJustDecodeBounds设为True时，不会真正加载图片，而是得到图片的宽高信息。
+        osts.inJustDecodeBounds=false;
+        Bitmap bitmap= BitmapFactory.decodeFile(filePath,osts);
+        ByteArrayOutputStream stream =new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+        try {
+            if (file.exists()){
+                file.delete();
+            }else{
+                file.createNewFile();
+            }
+            FileOutputStream fileOutputStream=new FileOutputStream(file);
+            fileOutputStream.write(stream.toByteArray());
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    
+    
+    
 }
